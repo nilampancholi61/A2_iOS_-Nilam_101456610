9084be5a16da0fe3f3057549922f7097a0ae5cb5
nilampancholi61 <155118869+nilampancholi61@users.noreply.github.com>
2025-03-30 19:47:37 -0400

    test

---
+++ A2_iOS_ Nilam_101456610/A2_iOS_ Nilam_101456610Tests/A2_iOS__Nilam_101456610Tests.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS__Nilam_101456610Tests.swift
+//  A2_iOS_ Nilam_101456610Tests
+//
+//  Created by Ankit Pancholi on 2025-03-30.
+//
+
+import Testing
+@testable import A2_iOS__Nilam_101456610
+
+struct A2_iOS__Nilam_101456610Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
